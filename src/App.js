dcb76938bcc9b1ffad52e8d3c2867c0fac5a8468
xsebastianx <sarnavat1@gmail.com>
2020-12-07 00:05:04 -0500

    Creating Articles - Frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Article from './components/articles/Article';
+import Articles from './components/articles/Articles';
+import ArticleForm from './components/articles/ArticleForm';
 
-export const App = () => {
+export default function App() {
   return (
     <div>
+      <Router>
+        <Container>
+          <Switch>
+            <Route path='/articles/new' component={ArticleForm} />
+            <Route path='/articles/:articleId/edit' component={ArticleForm} />
+            <Route path='/articles/:articleId' component={Article} />
+            <Route path='/articles' component={Articles} />
+          </Switch>
+        </Container>
+      </Router>
 
     </div>
   )
 }
+
+
--- src/components/articles/ArticleForm.jsx
+++ src/components/articles/ArticleForm.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+
 
 const ArticleForm = () => {
+    const initialState = { title: '', text: '' };
+    const [values, setValues] = useState(initialState);
+
+    const handleSubmit = e => {
+        e.preventDefualt()
+        fetch('/articles', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values)
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert('Article successfully created')
+                    setValues(initialState)
+                }
+            })
+            .catch(error => alert(error))
+    }
+
     return (
         <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId='formBasicEmail'>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type='text' placeholder='Title of your article...' required={true} onChange={e => setValues({ ...values, title: e.target.value })} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Text</Form.Label>
+                    <Form.Control type='texterea' rows='5' placeholder='Text for your article...' required={true} onChange={e => setValues({ ...values, text: e.target.value })} />
+                </Form.Group>
 
+
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
